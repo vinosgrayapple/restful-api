9d4f5065d41bad9e8d8a842a4fa69b44500dfde1
Komarichev Serg <komarichev@gmail.com>
2017-06-13 15:24:39 +0300

    restfull api template

---
+++ server.js
@@ -0,0 +1,14 @@
+const express    = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app        = express(); 
+const db         = require('./config/db');
+
+//server
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database)=> {
+	if (err) return console.log(err);
+	require('./app/routes')(app, database);
+	app.listen(port,()=>console.log('We are live on %s', port));
+});
